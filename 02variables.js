ac94d08fb207550e2456f92385da009db5596b51
Sarika Mohanraj <sarikamohan08@gmail.com>
2023-03-27 22:56:31 +0530

    variables

---
+++ 02variables.js
@@ -0,0 +1,19 @@
+const uid = "abc123";
+var fullName="sarika mohanraj";
+console.log(fullName);
+var courseName="MSC.AIML";
+console.log(courseName);
+var isLoggedIn =true;
+console.log(isLoggedIn);
+var LoggedCount=8;
+console.log(LoggedCount);
+var PaymentMode;
+
+PaymentMode="UPI";
+
+console.log(PaymentMode);
+console.log(`
+id:${uid}
+name: ${fullName}
+course Name:${courseName}
+`);
