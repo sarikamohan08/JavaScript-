c4a8a4400bbfb77e913d9347f78373b98f853332
Sarika Mohanraj <sarikamohan08@gmail.com>
2023-03-27 22:57:29 +0530

    context

---
+++ 06js_context.js
@@ -0,0 +1,15 @@
+// sayHello();---> global context
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+
+if (2 === "2") {
+    console.log("This is true");
+  }
+  
+  var myName = "sarika";
+  if (myName === window.myName) {
+    console.log("This is again a true statement");
+  }
+  
