0230c3da542fdad30c88a3a43f302e9f1fb47d33
Sarika Mohanraj <sarikamohan08@gmail.com>
2023-03-29 00:00:30 +0530

    advance objects

---
+++ 17_advancedObjects.js
@@ -0,0 +1,28 @@
+var User = function (firstName, courseCount) {
+    this.firstName = firstName;
+    this.courseCount = courseCount;
+    this.getCourseCount = function () {
+      console.log(`Course count is: ${this.courseCount}`);
+    };
+  };
+  
+  User.prototype.getFirstname = function () {
+    console.log(`Your firstname is : ${this.firstName}`);
+  };
+  
+  var sarika = new User("sarika", 2);
+  console.log(sarika);
+  sarika.getFirstname();
+  sarika.getCourseCount();
+  
+  if (sarika.hasOwnProperty("firstName")) {
+    sarika.getFirstname();
+  }
+  
+  //console.log(hitesh);
+  
+  var sam = new User("Sam", 1);
+  sam.getCourseCount();
+  sam.getFirstname();
+  //console.log(sam);
+  
