e242b43bd403f061536118a16645ef775f2546fd
Sarika Mohanraj <sarikamohan08@gmail.com>
2023-03-28 21:01:45 +0530

    objects

---
+++ 13_objects.js
@@ -0,0 +1,16 @@
+var user = {
+    firstName: "sarika",
+    lastName: "Mohanraj",
+    role: "Admin",
+    loginCount: 32,
+    facebookSignedIn: true,
+  };
+  
+  console.log(user.firstName);
+  console.log(user["lastName"]);
+  
+  console.log(user.loginCount);
+  user.loginCount = 44;
+  console.log(user.loginCount);
+  console.table(user);
+  
