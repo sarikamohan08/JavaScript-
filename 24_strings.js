4ea8cfd3d3b12dc4f4c5026fe6b5da61c76ad7de
Sarika Mohanraj <sarikamohan08@gmail.com>
2023-03-29 12:08:23 +0530

    strings

---
+++ 24_strings.js
@@ -0,0 +1,8 @@
+var name = `sarika`;
+var fullName = `${name} mohan`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("sarika"));
+console.log(fullName.toUpperCase());
